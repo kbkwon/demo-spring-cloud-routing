0b9c4a7dbd8295cc1c526aee9b279773b09dc948
Benjamin Kwon <benjamin.kwon.80@gmail.com>
2020-08-15 16:57:18 +0900

    Initial Commit

---
+++ src/main/java/com/github/kbkwon/demo/spring/cloud/routing/DemoSpringCloudRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.github.kbkwon.demo.spring.cloud.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCloudRoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudRoutingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kbkwon/demo/spring/cloud/routing/DemoSpringCloudRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kbkwon.demo.spring.cloud.routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudRoutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
